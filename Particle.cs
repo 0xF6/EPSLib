a805e8a09472cbde75c12431cf9437a56bebd868
Yuuki Wesp <ls-micro@ya.ru>
2019-01-21 03:51:50 +0300

    Create Particle.cs

---
+++ Particle.cs
@@ -0,0 +1,14 @@
+namespace EPS
+{
+    public class Particle
+    {
+        /// <summary>
+        /// Mass
+        /// </summary>
+        public double M { get; protected set; }
+        /// <summary>
+        /// Energy
+        /// </summary>
+        public double E { get; protected set; }
+    }
+}
