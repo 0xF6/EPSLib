5b2acfb40c33c6eb8bc6d1a407ad5e46f579a7bd
Yuuki Wesp <ls-micro@ya.ru>
2019-01-21 03:51:36 +0300

    Create global.cs

---
+++ global.cs
@@ -0,0 +1,13 @@
+namespace System.Internal
+{
+    /// <summary>
+    /// Global constats
+    /// </summary>
+    public class global
+    {
+        public static readonly double d = Math.Pow(10, -27);
+        public static readonly double h = 1.054572E-0 * d;
+        public static readonly double c = 299_792_458;
+        public static readonly double ln2 = 0.693E-0;
+    }
+}
