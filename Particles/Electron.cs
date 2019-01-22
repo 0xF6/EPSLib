4ef6138eb02a76085e33fdf650a12ab4cba720cc
Yuuki Wesp <ls-micro@ya.ru>
2019-01-22 05:01:35 +0300

    add electron

---
+++ Particles/Electron.cs
@@ -0,0 +1,17 @@
+﻿namespace EPS.Particles
+{
+    using System;
+    using System.Internal;
+
+    public class Electron : Particle, Lepton
+    {
+        public Electron()
+        {
+            t = LifeTime.Infinity;
+            E = -1.6021766208 * 1E-19;
+            m = -9.274009994 * 1E-24;
+            M = 5.48579909070 * 1E-4;
+            L = 1E-0;
+        }
+    }
+}
