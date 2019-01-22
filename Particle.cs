14161ff91fa992ad2a04b154f6722e69856a927e
Yuuki Wesp <ls-micro@ya.ru>
2019-01-22 04:03:29 +0300

    Update Particle.cs

--- Particle.cs
+++ Particle.cs
@@ -10,5 +10,48 @@ namespace EPS
         /// Energy
         /// </summary>
         public double E { get; protected set; }
+        /// <summary>
+        /// Electric charge
+        /// </summary>
+        public double Q { get; protected set; }
+        /// <summary>
+        /// Magnetic moment
+        /// </summary>
+        public double m { get; protected set; }
+        /// <summary>
+        /// Baryon charge
+        /// </summary>
+        public double B { get; protected set; }
+        /// <summary>
+        /// Lepton charge
+        /// </summary>
+        public double L { get; protected set; }
+        /// <summary>
+        /// Strangeness
+        /// </summary>
+        public long S { get; protected set; }
+        /// <summary>
+        /// Isotopic spin
+        /// </summary>
+        public QuantumState Iz { get; protected set; }
+        /// <summary>
+        /// G-parity
+        /// </summary>
+        public double ηG { get; protected set; }
+        /// <summary>
+        /// Super parity
+        /// </summary>
+        public double ηR { get; protected set; }
+        /// <summary>
+        /// Spin
+        /// </summary>
+        public QuantumState Sp { get; protected set; }
+
+
+
+        /// <summary>
+        /// Stable particle?
+        /// </summary>
+        public bool IsStable { get; protected set; }
     }
 }
