4820195d951ebd1ecdff4ea990dcd45e6a807eb5
Yuuki Wesp <ls-micro@ya.ru>
2019-01-22 04:33:40 +0300

    add photon

---
+++ Particles/Photon.cs
@@ -0,0 +1,18 @@
+﻿namespace EPS.Particles
+{
+    using System;
+
+    public class Photon : Particle, Boson
+    {
+        public Photon()
+        {
+            Sp = new QuantumState(1);
+            IsStable = true;
+            t = LifeTime.Stable;
+            M = 1E-27;
+            E = 1E-35;
+        }
+    }
+
+    
+}
