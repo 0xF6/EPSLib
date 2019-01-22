94c5e8b8d3163d095797bc8a3c169719e156e414
Yuuki Wesp <ls-micro@ya.ru>
2019-01-22 04:33:14 +0300

    add quantum state

---
+++ QuantumState.cs
@@ -0,0 +1,33 @@
+﻿namespace EPS
+{
+    using System;
+
+    public enum QBit
+    {
+        Negative = -1,
+        Zero,
+        Positive
+    }
+    public struct QuantumState
+    {
+        public QBit Value;
+        
+        public QuantumState(int value = 0)
+        {
+            switch (value)
+            {
+                case -1:
+                    Value = QBit.Negative;
+                    break;
+                case 1:
+                    Value = QBit.Positive;
+                    break;
+                case 0:
+                    Value = QBit.Zero;
+                    break;
+                default:
+                    throw new IndexOutOfRangeException($"QBit value can't set '{value}'");
+            }
+        }
+    }
+}
