34b9da1b708f10e83eaf31126e4dba7982ebf85d
Yuuki Wesp <ls-micro@ya.ru>
2019-01-22 05:07:05 +0300

    fix global and add infinity life time

--- LifeTime.cs
+++ LifeTime.cs
@@ -3,5 +3,6 @@
     public class LifeTime
     {
         public static readonly double Stable = double.NegativeInfinity;
+        public static readonly double Infinity = 1d / 0d;
     }
 }
--- global.cs
+++ global.cs
@@ -1,13 +1,23 @@
-namespace System.Internal
+﻿namespace System.Internal
 {
+    using EPS;
+    using EPS.Particles;
+
     /// <summary>
     /// Global constats
     /// </summary>
     public class global
     {
-        public static readonly double d = Math.Pow(10, -27);
+        public static readonly double d = 1E-27;
         public static readonly double h = 1.054572E-0 * d;
         public static readonly double c = 299_792_458;
         public static readonly double ln2 = 0.693E-0;
+
+
+        public static void Sim()
+        {
+            new Photon();
+            new Electron();
+        }
     }
 }
